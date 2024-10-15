eb7f4774b723e30aff81d2ec6407d9bfb9adfabd
gotomars <751081818@qq.com>
2024-10-15 22:18:15 +0900

    init slack project

---
+++ src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+// import { useState } from "react";
+// import { SignInFlow } from "../types";
+
+export const AuthScreen = () => {
+  //   const [state, setState] = useState<SignInFlow>("signIn");
+
+  return (
+    <div className="h-full flex items-center justify-center bg-[#5C3B58] text-white-400 text-2xl">
+      <div className="md:h-auto md:w-[420px]">Auth Screen</div>
+    </div>
+  );
+};
