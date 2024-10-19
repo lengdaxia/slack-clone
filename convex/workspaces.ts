31e42a1abded0623bc85b8ad79f1d3a6deca2b45
gotomars <751081818@qq.com>
2024-10-19 18:16:33 +0900

    Add Member entity and table

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,12 +1,31 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+import { useId } from "react";
 
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+    if (!useId) {
+        return[];
+    }
+
+    const members = await ctx.db.query("members")
+    .withIndex("by_user_id", (q) => q.eq("userId", userId!))
+    .collect()
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+    const workspaces = [];
+    for(const wkId of workspaceIds) {
+      const wk = await ctx.db.get(wkId);
+      if (wk) {
+        workspaces.push(wk);
+      }
+    }
+
+    return workspaces;
   }
 })
 
@@ -16,12 +35,19 @@ export const create = mutation({
   },  
   handler: async (ctx, args) => {
     const userId = await checkAndGetUserId(ctx);
-    const joinCode = "123456";
+    const joinCode = generateJoinCode();
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode
     });
+
+    // join the workspace member when create a workspace
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin"
+    })
 
     return workspaceId;
   }
@@ -30,7 +56,17 @@ export const create = mutation({
 export const getById = query({
   args: {id: v.id("workspaces")},
   handler: async (ctx, args)  => {
-      await checkAndGetUserId(ctx);
+      const userId = await checkAndGetUserId(ctx);
+
+      const member = await ctx.db.query("members")
+      .withIndex("by_workspace_id_user_id", (q) => 
+        q.eq("workspaceId", args.id).eq("userId", userId))
+      .unique();
+
+      if (!member) {
+        return null;
+      }
+
       return await ctx.db.get(args.id);
   },
 })
@@ -41,4 +77,12 @@ const checkAndGetUserId = async (ctx: any) => {
     throw Error("Unauthorized")    
   }
   return userId;
+}
+
+const generateJoinCode = () => {
+  const code = Array.from(
+    {length: 6},
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+  return code;
 }
